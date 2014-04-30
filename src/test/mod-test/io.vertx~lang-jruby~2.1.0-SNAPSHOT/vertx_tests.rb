d830c87948d7cfc4df7960ffb73fc83db77d49f6
Paolo Marino <paolo.marino@spindox.it>
2014-04-30 03:02:40 +0200

    clean

--- src/test/mod-test/io.vertx~lang-jruby~2.1.0-SNAPSHOT/vertx_tests.rb
+++
@@ -1,8 +0,0 @@
-VertxAssert = Java::OrgVertxTesttools::VertxAssert
-
-VertxAssert.initialize(org.vertx.java.platform.impl.JRubyVerticleFactory.vertx)
-
-def start_tests(top)
-  method_name = Vertx.config['methodName']
-  self.send(method_name)
-end
